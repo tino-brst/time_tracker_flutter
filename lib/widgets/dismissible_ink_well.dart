1a351e44fe87b5580381edc0182d450343b55ea6
Agustin Burset <agustin.burset@icloud.com>
2020-01-20 09:44:15 -0300

    Add Dismissible InkWell widget, require on widgets extending Dismissible

--- lib/widgets/dismissible_ink_well.dart
+++ lib/widgets/dismissible_ink_well.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/material.dart';
 
-class DismissibleListTile extends StatelessWidget {
+class DismissibleInkWell extends StatelessWidget {
   final Key key;
   final VoidCallback onTap;
   final VoidCallback onDismissed;
-  final String title;
+  final Widget child;
 
-  const DismissibleListTile({
+  const DismissibleInkWell({
     @required this.key,
+    this.child,
     this.onTap,
     this.onDismissed,
-    this.title,
   });
 
   @override
   Widget build(BuildContext context) {
-    final titleWidget = title == null ? null : Text(title);
-    final trailingIcon = Icon(Icons.chevron_right);
-
     return Dismissible(
       key: key,
       direction: DismissDirection.endToStart,
       background: Container(color: Colors.red),
       onDismissed: (_) => onDismissed(),
-      child: ListTile(
-        title: titleWidget,
+      child: InkWell(
         onTap: onTap,
-        contentPadding: EdgeInsets.only(left: 16, right: 10),
-        trailing: trailingIcon,
+        child: child,
       ),
     );
   }
