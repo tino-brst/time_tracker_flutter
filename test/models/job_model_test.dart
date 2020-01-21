42ab08e9834471ec8a5cfd7c75e764b9d1e3f336
Agustin Burset <agustin.burset@icloud.com>
2020-01-21 19:37:21 -0300

    Add JobModel convertion methods unit tests

---
+++ test/models/job_model_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:time_tracker/models/job_model.dart';
+
+void main() {
+  group('JobModel', () {
+    test('.fromDocumentDataAndId creates a JobModel with the correct properties', () {
+      const documentData = {'name': 'bar', 'ratePerHour': 42};
+      const documentId = 'foobar';
+
+      final actual = JobModel.fromDocumentDataAndId(documentData, documentId);
+      final expected = JobModel(id: 'foobar', name: 'bar', ratePerHour: 42);
+
+      expect(actual, equals(expected));
+    });
+
+    test('toDocumentData creates a Map with the key-value pairs', () {
+      final actual = JobModel(id: 'foobar', name: 'bar', ratePerHour: 42).toDocumentData();
+      final expected = {'name': 'bar', 'ratePerHour': 42};
+
+      expect(actual, equals(expected));
+    });
+  });
+}
