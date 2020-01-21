e316ee977225bedf36cd14a0f1e6a1f0cbfa5bc7
Agustin Burset <agustin.burset@icloud.com>
2020-01-21 15:26:18 -0300

    Add NonEmptyStringValidator tests

---
+++ test/validators_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:time_tracker/core/validators.dart';
+
+void main() {
+  group('NonEmptyStringValidator', () {
+    test('considers valid a non empty string', () {
+      final validator = NonEmptyStringValidator();
+      expect(validator.isValid('foobar'), true);
+    });
+    test('considers invalid an empty string', () {
+      final validator = NonEmptyStringValidator();
+      expect(validator.isValid(''), false);
+    });
+    test('considers invalid a null input', () {
+      final validator = NonEmptyStringValidator();
+      expect(validator.isValid(null), false);
+    });
+  });
+}
